a9c088cbd35b453413ce1852343cfa2d98491ba1
Chris Merrill <mthe2845@gmail.com>
2022-07-30 15:23:30 -0500

    added new images

--- src/images.js
+++ src/images.js
@@ -1,8 +1,8 @@
 const images = (() => {
 
-    let arr = ["./dist/images/Abuelos-Mexican-Restaurant-1.jpg",
-     "./dist/images/Restaurant-meal-1024x680.jpg",
-     "./dist/images/190509-coconut-chicken-curry-157-1558039780.jpg"];
+    let arr = ["https://www.theplazarestaurant.com/wp-content/uploads/2018/05/Plaza-36-min-1024x768.jpg",
+     "https://cdn.vox-cdn.com/thumbor/Mw5AQde8wqT_Vaqk-LMR0QmSc2g=/0x0:960x635/1200x800/filters:focal(404x242:556x394)/cdn.vox-cdn.com/uploads/chorus_image/image/54943775/the_bird_hamburg.0.jpg",
+     "https://hips.hearstapps.com/hmg-prod/images/190509-coconut-chicken-curry-157-1558039780.jpg"];
 
      return {arr};
 
