79d85b37752fa7a04895a6d8de8c27e01775da05
Christopher Merrill <47538097+Revan20911@users.noreply.github.com>
2022-07-30 14:32:25 -0500

    Update images.js

--- src/images.js
+++ src/images.js
@@ -1,8 +1,8 @@
 const images = (() => {
 
-    let arr = ["/dist/images/Abuelos-Mexican-Restaurant-1.jpg",
-     "/dist/images/Restaurant-meal-1024x680.jpg",
-     "/dist/images/190509-coconut-chicken-curry-157-1558039780.jpg"];
+    let arr = ["./dist/images/Abuelos-Mexican-Restaurant-1.jpg",
+     "./dist/images/Restaurant-meal-1024x680.jpg",
+     "./dist/images/190509-coconut-chicken-curry-157-1558039780.jpg"];
 
      return {arr};
 
